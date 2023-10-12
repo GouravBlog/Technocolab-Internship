6540e0a8ac669a10d226efe6b309ddd86845a493
Gourav wasale <103139462+GouravBlog@users.noreply.github.com>
2023-10-12 20:15:30 +0530

    Create index.js

---
+++ JAVASCRIPT PROJECT TASK/Project Task 7/index.js
@@ -0,0 +1,19 @@
+// myage variable create
+let myAge = 24;
+
+// earlyyears variable create
+let earlyYears = 2;
+
+earlyYears = earlyYears*10.5;
+
+// laterYears variable decleared
+let laterYears = myAge-2;
+
+// add earlyyears and lateryears variable
+let myAgeInDogYears = earlyYears + laterYears;
+
+let myName = "gourav wasale"
+// my age variavle character set to lowercase
+myName = myName.toLowerCase();
+
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`)
