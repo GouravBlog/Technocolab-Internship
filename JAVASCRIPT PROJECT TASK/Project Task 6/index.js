81be9fd3a4b6b001e2a29134b42462b9e81f0fcf
Gourav wasale <103139462+GouravBlog@users.noreply.github.com>
2023-10-12 20:15:16 +0530

    Create index.js

---
+++ JAVASCRIPT PROJECT TASK/Project Task 6/index.js
@@ -0,0 +1,22 @@
+// variable name kelvin create
+let kelvin = 293;
+
+// variable name celsius create
+let celsius = kelvin-273;
+
+// variable name fahrenheit create and store the equation
+let fahrenheit = celsius * (9/5) + 32;
+
+// variable name fahrenheit round the decimal value
+fahrenheit = Math.floor(fahrenheit)
+
+
+console.log(`The temperature is ${fahrenheit} degrees Fahrenheit.`)
+
+// variable name newton create and store the equation
+newton = celsius * (33/100)
+
+// variable name newton round the decimal value
+newton = Math.floor(newton)
+
+console.log(`The temperature is ${newton} newton.`)
